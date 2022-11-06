2bcdd337681056eb31fa4aa621f10843f3bc1b91
Jacob Truong <jacob.truong@ymail.com>
2022-11-06 11:18:27 -0800

    test

--- server/server.js
+++ server/server.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const app = express();
+const port = 8000;
+
+
+app.get('/api', (req, res) => res.json("Hello, world!"));
+
+
+// Start server 
+app.listen(port, () => console.log(`Listening on port ${port}`));
