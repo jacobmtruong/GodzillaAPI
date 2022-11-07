7e7e4be7d9114e3056f81092726f306fd1e7ac7b
Jacob Truong <jacob.truong@ymail.com>
2022-11-07 11:36:29 -0800

    monster model

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-
 mongoose.connect("mongodb://localhost/godzillaAPI", {
     useNewUrlParser: true,
     useUnifiedTopology: true
---
+++ server/models/monster.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const MonsterSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    imgUrl: {
+        type: String,
+        required: true
+    },
+    appearance: {
+        type: String,
+        required: true
+    },
+    level: {
+        type: String,
+        required: true
+    },
+    yearCreated: {
+        type: Number,
+        required: true
+    },
+    link: {
+        type: String
+    }
+}, {timestamps: true});
+
+// Create a new MonsterSchema and export it 
+const Monster = mongoose.model('Monster', MonsterSchema);
+module.exports = Monster;
