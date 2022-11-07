7c23e2fda54030e4eb5609589b444748c46c090b
Jacob Truong <jacob.truong@ymail.com>
2022-11-07 12:10:44 -0800

    cruds controller

---
+++ server/controllers/monster.controller.js
@@ -0,0 +1,40 @@
+// Bring in monster model
+const Monster = require('../models/monster.model');
+
+module.exports = {
+
+    // Read ALL
+    findAll: (req, res) => {
+        Monster.find()
+            .then(allMonsters => res.json(allMonsters))
+            .catch(err => res.json({ message: 'There was an error', serverError: err }))
+    },
+
+    // Create a new monster
+    createMonster: (req, res) => {
+        Monster.create(req.body)
+            .then(newMonster => res.json(newMonster))
+            .catch(err => res.json({ message: 'There was an error', serverError: err }))
+    },
+
+    // find ONE
+    findMonster: (req, res) => {
+        Monster.findById(red.params.id)
+            .then(monster => res.json(monster))
+            .catch(err => res.json({ message: 'There was an error', serverError: err }))
+    },
+
+    //update a monster
+    updateMonster: (req, res) => {
+        Monster.findByIdAndUpdate(req.params.id, red.body, { new: true, runValidators: true })
+            .then(updatedMonster => res.json(updatedMonster))
+            .catch(err => res.json({ message: 'There was an error', serverError: err }))
+    },
+
+    // delete a monster
+    deleteMonster: (req, res) => {
+        Monster.findByIdAndDelete(req.params.id)
+        .then(deleteConfirme => res.json(deleteConfirme))
+        .catch(err => res.json({ message: 'There was an error', serverError: err }))
+    }
+}
