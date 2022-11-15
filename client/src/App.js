8200766d329f25547e0b0d1015c7555417f7a036
Jacob Truong <jacob.truong@ymail.com>
2022-11-15 13:15:19 -0800

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from 'react-router-dom'
+import FormInput from './Components/FormInput';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <FormInput/>
     </div>
   );
 }
---
+++ client/src/Components/FormInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {axios} from 'axios'
+
+const FormInput = () => {
+  return (
+    <div>
+        this is a form
+    </div>
+  )
+}
+
+export default FormInput
